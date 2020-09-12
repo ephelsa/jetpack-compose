d2f5d20b793969f8f83816007bd1cac05000d94c
Leonardo A. Perez Castilla <xephelsax@gmail.com>
2020-09-12 01:56:26 -0500

    Basic 1. Composable functions

---
+++ app/src/main/java/com/ephelsa/github/composebasics/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.ephelsa.github.composebasics
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Greeting(name = "Leonardo")
+        }
+    }
+}
+
+// 1. Composable functions
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name")
+}
+
+@Preview
+@Composable
+fun PreviewGreeting() {
+    Greeting("Leonardo")
+}
