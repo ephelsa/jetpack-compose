3181f19c1c0590cbf47aedd9c433d8db52af0460
Leonardo A. Perez Castilla <xephelsax@gmail.com>
2020-09-12 17:51:58 -0500

    Recomposition skips as much as possible

--- app/src/main/java/com/ephelsa/github/composebasics/MainActivity.kt
+++ app/src/main/java/com/ephelsa/github/composebasics/MainActivity.kt
@@ -1,18 +1,24 @@
 package com.ephelsa.github.composebasics
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.material.Button
+import androidx.compose.foundation.lazy.LazyColumnFor
+import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,31 +26,51 @@ class MainActivity : AppCompatActivity() {
         private val TAG = MainActivity::class.java.simpleName
     }
 
+    private val petList = mutableStateListOf(
+        "Dog 🐶",
+        "Cat 😸",
+        "Mouse 🐭",
+        "Rabbit 🐰"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column {
-                ClickCounterStateful()
-                Spacer(modifier = Modifier.height(20.dp))
-                ClickCounterStateful()
+            PetList(header = "Beauty pets", petList = petList) {
+                Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
             }
+        }
+
+        GlobalScope.launch {
+            updatePetList()
+        }
+    }
+
+    private suspend fun updatePetList() {
+        delay(2000)
+        petList.add("Fox 🦊")
+    }
+}
+
+@Composable
+fun PetList(
+    header: String,
+    petList: List<String>,
+    onSelected: (String) -> Unit
+) {
+    Column {
+        Text(text = header)
+        Spacer(modifier = Modifier.height(20.dp))
+        LazyColumnFor(items = petList) { pet ->
+            PetItem(pet = pet, onClick = onSelected)
         }
     }
 }
 
 @Composable
-fun ClickCounter(clicks: Int, onClick: () -> Unit) {
-    Button(onClick = onClick) {
-        Text("I've been clicked $clicks times")
-    }
-}
-
-@Composable
-fun ClickCounterStateful() {
-    val counterState = remember { mutableStateOf(0) }
-
-    ClickCounter(
-        clicks = counterState.value,
-        onClick = { counterState.value += 1 }
+fun PetItem(pet: String, onClick: (String) -> Unit) {
+    Text(
+        text = pet,
+        modifier = Modifier.clickable(onClick = { onClick(pet) })
     )
 }
