7c9d58d4e482b7db4b108cdc9640f084bac7705c
Leonardo A. Perez Castilla <xephelsax@gmail.com>
2020-09-12 02:04:14 -0500

    Basic 2. Layouts

--- app/src/main/java/com/ephelsa/github/composebasics/MainActivity.kt
+++ app/src/main/java/com/ephelsa/github/composebasics/MainActivity.kt
@@ -2,9 +2,19 @@ package com.ephelsa.github.composebasics
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
@@ -26,4 +36,42 @@ fun Greeting(name: String) {
 @Composable
 fun PreviewGreeting() {
     Greeting("Leonardo")
+}
+
+// 2. Layout
+@Composable
+fun NewsStory() {
+    val image = imageResource(R.drawable.header)
+
+    Column(
+        modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalGravity = Alignment.CenterHorizontally
+
+    ) {
+        val imageModifier = Modifier
+            .preferredHeight(180.dp)
+            .fillMaxWidth()
+
+        Image(
+            asset = image,
+            modifier = imageModifier,
+            alignment = Alignment.Center,
+            contentScale = ContentScale.Fit,
+            alpha = 0.9f,
+            colorFilter = ColorFilter(Color.Green, BlendMode.Overlay)
+        )
+
+        Spacer(modifier = Modifier.preferredHeight(16.dp))
+
+        Text("Jetpack Compose works on Canary AS version")
+        Text("Sincelejo, Sucre")
+        Text("September 2020")
+    }
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    NewsStory()
 }
